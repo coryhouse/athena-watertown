f66f1d65b5c9426e98654789a5fefd79343be86c
Cory House <housecor@gmail.com>
2019-11-06 15:11:41 -0500

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([
+    { id: 1, name: "Cory", email: "c@h.com" },
+    { id: 2, name: "Megan", email: "m@c.com" },
+    { id: 3, name: "Tami", email: "t@tonga.com" }
+  ]);
+
   const h1Style = {
     color: "red",
     marginBottom: 20
   };
 
-  const users = [
-    { id: 1, name: "Cory", email: "c@h.com" },
-    { id: 2, name: "Megan", email: "m@c.com" },
-    { id: 3, name: "Tami", email: "t@tonga.com" }
-  ];
+  function handleDelete(id) {
+    // Remove deleted element from users array
+    const newUsers = users.filter(user => user.id !== id);
+    setUsers(newUsers); // update state, so React knows to re-render
+  }
 
   return (
     <>
       <h1 className="header" style={h1Style}>
-        App
+        Users
       </h1>
+      <ul>
+        {users.map(user => (
+          <li>
+            {/* Delay execution via arrow func */}
+            <button onClick={event => handleDelete(user.id)}>Delete</button>
+            {user.name}
+          </li>
+        ))}
+      </ul>
       <label htmlFor="firstName">First Name</label>
       <input id="firstName" type="text"></input>
       <p>My app.</p>
