60ec46433d824abedfb490c99723fdedfac59954
Cory House <housecor@gmail.com>
2019-11-07 13:52:27 -0500

    AddUser proxy

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -19,3 +19,18 @@ export function deleteUser(userId) {
     throw new Error("Bad network response.");
   });
 }
+
+export function addUser(user) {
+  return fetch("http://localhost:3001/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error("Bad network response.");
+  });
+}
