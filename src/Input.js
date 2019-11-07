2a0ec40426f40ef533800a33ab44be81b97f8674
Cory House <housecor@gmail.com>
2019-11-07 15:26:39 -0500

    Add propTypes and defaultProps

--- src/Input.js
+++ src/Input.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Input(props) {
   return (
@@ -14,5 +15,17 @@ function Input(props) {
     </div>
   );
 }
+
+Input.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(["text", "email", "number", "password"]),
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired
+};
+
+Input.defaultProps = {
+  type: "text"
+};
 
 export default Input;
