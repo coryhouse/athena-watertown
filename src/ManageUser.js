8945e72a3e2b70c433db7cbfd48800578fb17d6c
Cory House <housecor@gmail.com>
2019-11-07 13:52:47 -0500

    Single change handler

--- src/ManageUser.js
+++ src/ManageUser.js
@@ -7,6 +7,12 @@ const ManageUser = () => {
   });
 
   function handleSubmit(event) {}
+
+  function handleUserChange(event) {
+    // Use computed property syntax to reference a property using a variable.
+    setUser({ ...user, [event.target.id]: event.target.value });
+  }
+
   return (
     <>
       <h1>Add User</h1>
@@ -14,13 +20,23 @@ const ManageUser = () => {
         <div>
           <label htmlFor="name">Name</label>
           <br />
-          <input id="name" type="text" value={user.name}></input>
+          <input
+            id="name"
+            type="text"
+            onChange={handleUserChange}
+            value={user.name}
+          ></input>
         </div>
 
         <div>
           <label htmlFor="email">Email</label>
           <br />
-          <input id="email" type="email" value={user.email}></input>
+          <input
+            id="email"
+            type="email"
+            onChange={handleUserChange}
+            value={user.email}
+          ></input>
         </div>
 
         <input type="submit" value="Add User" />
