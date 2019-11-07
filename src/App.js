bf7ae757c1c5591566f32a91a6011cb03d5cbd46
Cory House <housecor@gmail.com>
2019-11-07 11:34:31 -0500

    Configure routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Users from "./Users";
+import Home from "./Home";
+import ManageUser from "./ManageUser";
+import { Route } from "react-router-dom";
 
 function App() {
-  return <Users />;
+  return (
+    <>
+      {/* When the URL is at root, load the users component. */}
+      <Route path="/" component={Home} exact />
+      <Route path="/users" component={Users} />
+      <Route path="/user" component={ManageUser} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/ManageUser.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ManageUser = () => {
+  return <h1>Add User</h1>;
+};
+
+export default ManageUser;
