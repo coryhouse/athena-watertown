535d61734ef909ef130eedeacae1a3947dfab92c
Cory House <housecor@gmail.com>
2019-11-07 15:58:42 -0500

    Add route

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       {/* When the URL is at root, load the users component. */}
       <Route path="/" component={Home} exact />
       <Route path="/users" component={Users} />
-      <Route path="/user" component={ManageUser} />
+      <Route path="/user/:userId?" component={ManageUser} />
     </>
   );
 }
