1f39deea65b9a0227d873bc5577b541455eded54
Cory House <housecor@gmail.com>
2019-11-08 10:11:44 -0500

    WIP

--- src/ManageUser.js
+++ src/ManageUser.js
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { addUser } from "./api/userApi";
-import { Redirect } from "react-router-dom";
+import { Redirect, useRouteMatch } from "react-router-dom";
 import Input from "./Input";
 
 const ManageUser = () => {
+  const match = useRouteMatch(); // info about the matching URL
+  const { userId } = match.params;
+
   const [user, setUser] = useState({
     name: "",
     email: ""
   });
   const [saveCompleted, setSaveCompleted] = useState(false);
+
+  useEffect(() => {
+    if (userId) {
+      // Get the userId out of props.users
+    }
+  });
 
   async function handleSubmit(event) {
     event.preventDefault(); // Stop browser from posting back
